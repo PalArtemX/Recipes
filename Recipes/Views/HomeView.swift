f735f849fcac235106ee2ebe3ddc2748897a6b41
Artem Paliutin <pal.artem@icloud.com>
2022-02-21 18:24:07 +0300

    start

---
+++ Recipes/Views/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  Recipes
+//
+//  Created by Artem Paliutin on 21.02.2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
