2c3e0a1f72b0ff579df6731b77c4c5246794a6df
Artem Paliutin <pal.artem@icloud.com>
2022-02-21 18:19:21 +0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Artem Paliutin on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
